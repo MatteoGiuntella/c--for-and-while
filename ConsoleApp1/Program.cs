95dd31d7d9a96f739e57823fb0d8b985d0063cb8
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-05-01 22:18:53 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        Console.WriteLine("Inserisci un numero intero:");
+        int numeroInserito;
+
+        // Ciclo per assicurarmi che l'input sia un numero intero 
+        while (!int.TryParse(Console.ReadLine(), out numeroInserito))
+        {
+            Console.WriteLine("Input non valido. Inserisci un numero intero:");
+        }
+
+        // Ciclo per stampare la tabellina 
+        for (int tab = 1; tab <= 10; tab++)
+        {
+            int risultato = numeroInserito * tab;
+            Console.WriteLine($"{numeroInserito} x {tab} = {risultato}");
+        }
